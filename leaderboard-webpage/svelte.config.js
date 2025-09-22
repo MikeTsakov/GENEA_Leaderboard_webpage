13f4eb591cca1b470c33a192ec084edca79a895c
Mihail <tsakovm@gmail.com>
2025-09-22 15:30:32 +0200

    initial setup and locally running

---
+++ leaderboard-webpage/svelte.config.js
@@ -0,0 +1,30 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+
+const dev = process.argv.includes('dev');
+const base = dev ? '' : '/GENEA_Leaderboard_webpage'; // 👈 replace with your repo name
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://svelte.dev/docs/kit/integrations
+	// for more information about preprocessors
+	preprocess: vitePreprocess(),
+
+	kit: {
+		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
+		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
+		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base
+		}
+	}
+};
+
+export default config;
